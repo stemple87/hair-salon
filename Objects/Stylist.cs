352aa5aedab89e910148865bddf8084b455f5b7b
simon <simonctemple@live.com>
2016-02-26 09:33:52 -0800

    add database.cs first, first passing test

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -1,23 +1,183 @@
-using Xunit;
 using System.Collections.Generic;
 using System;
-using System.Data;
 using System.Data.SqlClient;
 
 namespace HairSalonNS
 {
-  public class CuisineTest : IDisposable
+  public class Stylist
   {
-    public CuisineTest()
+    private int _id;
+    private string _name;
+
+    public Stylist(string Name, int Id = 0)
     {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+      _id = Id;
+      _name = Name;
     }
 
-    public void Dispose()
+    public int GetId()
     {
-      Resturant.DeleteAll();
-      Cuisine.DeleteAll();
-
+      return _id;
     }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public override bool Equals(System.Object otherStylist)
+    {
+      if (!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        bool idEquality = this.GetId() == newStylist.GetId();
+        bool nameEquality = this.GetName() == newStylist.GetName();
+        return (idEquality && nameEquality);
+      }
+    }
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        Stylist newStylist = new Stylist(stylistName, stylistId);
+        allStylists.Add(newStylist);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allStylists;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO stylists (name) OUTPUT INSERTED.id VALUES (@StylistName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@StylistName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", conn);
+      cmd.ExecuteNonQuery();
+    }
+
+    public static Stylist Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists WHERE id = @StylistId;", conn);
+      SqlParameter stylistIdParameter = new SqlParameter();
+      stylistIdParameter.ParameterName = "@StylistId";
+      stylistIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(stylistIdParameter);
+      rdr = cmd.ExecuteReader();
+
+      int foundStylistId = 0;
+      string foundStylistDescription = null;
+
+      while(rdr.Read())
+      {
+        foundStylistId = rdr.GetInt32(0);
+        foundStylistDescription = rdr.GetString(1);
+      }
+      Stylist foundStylist = new Stylist(foundStylistDescription, foundStylistId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundStylist;
+    }
+
+    // public List<Client> GetClients()
+    // {
+    //   SqlConnection conn = DB.Connection();
+    //   SqlDataReader rdr = null;
+    //   conn.Open();
+    //
+    //   SqlCommand cmd = new SqlCommand("SELECT * FROM clients WHERE stylist_id = @StylistId;", conn);
+    //   SqlParameter stylistIdParameter = new SqlParameter();
+    //   stylistIdParameter.ParameterName = "@StylistId";
+    //   stylistIdParameter.Value = this.GetId();
+    //   cmd.Parameters.Add(stylistIdParameter);
+    //   rdr = cmd.ExecuteReader();
+    //
+    //   List<Client> clients = new List<Client> {};
+    //   while (rdr.Read())
+    //   {
+    //     int ClientId = rdr.GetInt32(0);
+    //     string ClientName = rdr.GetString(1);
+    //     DateTime ClientDateTime = rdr.GetDateTime(2);
+    //     string ClientLocation = rdr.GetString(3);
+    //     int ClientStylistId = rdr.GetInt32(4);
+    //     Client newClient = new Client(ClientName, ClientLocation, ClientStylistId, ClientDateTime, ClientId);
+    //     clients.Add(newClient);
+    //   }
+    //   if (rdr != null)
+    //   {
+    //     rdr.Close();
+    //
+    //   }
+    //   if (conn != null)
+    //   {
+    //     conn.Close();
+    //   }
+    //   return clients;
+    // }
+
   }
 }
